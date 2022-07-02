4860a329c5041a6a0d980ee20f5c2c7d0b488333
localhost <79594156+localhost-sys@users.noreply.github.com>
2022-07-02 16:39:30 +0300

    Add files via upload

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "Game"                  -- The title of the window the game is in (string)
+    t.version = "11.4"                    -- The LÖVE version this game was made for (string)
+    t.console = true                     -- Attach a console (boolean, Windows only)
+    t.window.width = 640                 -- The window width (number)
+    t.window.height = 480                 -- The window height (number)
+end
---
+++ player.lua
@@ -0,0 +1,36 @@
+
+Player = {}
+
+function Player:load()
+    self.x = 50
+    self.y = love.graphics.getHeight() / 2
+    self.width = 20
+    self.height = 100
+    self.speed = 500
+end
+
+
+function Player:update(dt)
+    self:move(dt)
+    self:CheckBoundaries(dt)
+end
+
+function Player:move(dt)    
+    if love.keyboard.isDown("w") then
+        self.y = self.y - self.speed * dt
+    elseif love.keyboard.isDown("s") then
+        self.y = self.y + self.speed * dt
+    end
+end
+
+function Player:CheckBoundaries(dt)
+    if self.y < 0 then
+        self.y = 0
+    elseif self.y + self.height > love.graphics.getHeight() then
+        self.y = love.graphics.getHeight() - self.height
+    end
+end
+
+function Player:draw()
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+end
